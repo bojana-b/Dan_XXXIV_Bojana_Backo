9eddc7f247418816ad3787f07353784e097593ba
bojana-b <66784194+bojana-b@users.noreply.github.com>
2020-06-18 12:15:22 +0200

    Add UI

--- Dan_XXXIV_Bojana_Backo/BankAcct.cs
+++ Dan_XXXIV_Bojana_Backo/BankAcct.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Dan_XXXIV_Bojana_Backo
 {
@@ -23,7 +19,7 @@ namespace Dan_XXXIV_Bojana_Backo
             if((Balance - amt) < 0)
             {
                 Console.WriteLine("{0} is trying to withdraw {1} RSD", Thread.CurrentThread.Name, amt);
-                Console.WriteLine("Sorry you are trying to withdraw {0} from ATM_1, but in Account is only {1}", amt, Balance);
+                Console.WriteLine("Sorry you are trying to withdraw {0} from ATM_1, but in Account is only {1} RSD", amt, Balance);
             }
             // Lock the critical section of code
             lock (acctLock)
@@ -31,7 +27,7 @@ namespace Dan_XXXIV_Bojana_Backo
                 if(Balance >= amt)
                 {
                     Console.WriteLine("{0} is trying to withdraw {1} RSD.", Thread.CurrentThread.Name, amt);
-                    Console.WriteLine("Removed {0} from ATM_1 and {1} left in Account.", amt, (Balance - amt));
+                    Console.WriteLine("Removed {0} RSD from ATM_1 and {1} RSD left in Account.", amt, (Balance - amt));
                     Balance -= amt;
                 }
             }
@@ -41,7 +37,7 @@ namespace Dan_XXXIV_Bojana_Backo
             if ((Balance - amt) < 0)
             {
                 Console.WriteLine("{0} is trying to withdraw {1} RSD.", Thread.CurrentThread.Name, amt);
-                Console.WriteLine("Sorry you are trying to withdraw {0} from ATM_2, but in Account is only {1}.", amt, Balance);
+                Console.WriteLine("Sorry you are trying to withdraw {0} RSD from ATM_2, but in Account is only {1} RSD.", amt, Balance);
             }
             // Lock the critical section of code
             lock (acctLock)
@@ -49,7 +45,7 @@ namespace Dan_XXXIV_Bojana_Backo
                 if (Balance >= amt)
                 {
                     Console.WriteLine("{0} is trying to withdraw {1} RSD", Thread.CurrentThread.Name, amt);
-                    Console.WriteLine("Removed {0} from ATM_2 and {1} left in Account", amt, (Balance - amt));
+                    Console.WriteLine("Removed {0} RSD from ATM_2 and {1} RSD left in Account", amt, (Balance - amt));
                     Balance -= amt;
                 }
             }
--- Dan_XXXIV_Bojana_Backo/Program.cs
+++ Dan_XXXIV_Bojana_Backo/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace Dan_XXXIV_Bojana_Backo
 {
@@ -12,17 +8,65 @@ namespace Dan_XXXIV_Bojana_Backo
         static void Main(string[] args)
         {
             Random random = new Random();
-            int numberOfClientsATM1 = 3;
-            int numberOfClientsATM2 = 5;
+            bool repeat = false;
+            int numberOfClientsATM1 = 0;
+            int numberOfClientsATM2 = 0;
             BankAcct acct = new BankAcct(10000);
+            do
+            {
+                Console.Write("\nEnter the NUMBER of clients for ATM_1 -> ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("\nYou must provide some input!");
+                    repeat = true;
+                }
+                else if (!Int32.TryParse(input, out numberOfClientsATM1))
+                {
+                    Console.WriteLine("Read instructions!!!!");
+                    repeat = true;
+                }
+                else
+                {
+                    numberOfClientsATM1 = Int32.Parse(input);
+                    repeat = false;
+                }
+            } while (repeat);
+
+            do
+            {
+                Console.Write("\nEnter the NUMBER of clients for ATM_2 -> ");
+                string input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("\nYou must provide some input!");
+                    repeat = true;
+                }
+                else if (!Int32.TryParse(input, out numberOfClientsATM2))
+                {
+                    Console.WriteLine("\nRead instructions!!!!");
+                    repeat = true;
+                }
+                else
+                {
+                    numberOfClientsATM2 = Int32.Parse(input);
+                    repeat = false;
+                }
+            } while (repeat);
+
+            Console.WriteLine();
+
+            // Number of Threads is equal to number of clients in both ATM
             Thread[] threads = new Thread[numberOfClientsATM1 + numberOfClientsATM2];
+
+            // Create Threads for clients for ATM1
             for (int i = 0; i < numberOfClientsATM1; i++)
             {
                 Thread t = new Thread(() => acct.WithdrawMachineOne(random.Next(100, 10001)));
                 threads[i] = t;
                 threads[i].Name = string.Format("Client_{0}_ATM1", i + 1);
-                
             }
+            // Create Threads for clients for ATM2
             for (int i = numberOfClientsATM1; i < numberOfClientsATM2 + numberOfClientsATM1; i++)
             {
                 Thread t = new Thread(() => acct.WithdrawMachineTwo(random.Next(100, 10001)));
